ca01784cef6404f79f58a1364f626df037f9e6cb
Tyler Sorenson <tylersorenson@Tylers-MacBook-Air.local>
2016-09-28 17:19:41 -0700

    Set up item list

--- Scripts/mkaul.module.js
+++ Scripts/mkaul.module.js
@@ -3,4 +3,48 @@ var myapp = angular.module('mkaul', []);
 myapp.controller('homeController', function($scope) {
 	var vm = this;
 	$scope.display = "Welcome to Mkaul Grace's corner";
+    
+    $scope.items = [
+        {
+            src: 'Images/IMG_6403.jpg',
+            name: 'Indie',
+            title: 'Beautiful Bib',
+            description: 'Take this lovely bad boy for a spin'
+        },
+        {
+            src: 'Images/IMG_6377.jpg',
+            name: 'Indie',
+            title: 'Beautiful Bib',
+            description: 'Take this lovely bad boy for a spin'
+        },
+        {
+            src: 'Images/IMG_6614.jpg',
+            name: 'Indie',
+            title: 'Beautiful Bib',
+            description: 'Take this lovely bad boy for a spin'
+        },
+        {
+            src: 'Images/IMG_6403.jpg',
+            name: 'Indie',
+            title: 'Beautiful Bib',
+            description: 'Take this lovely bad boy for a spin'
+        },
+        {
+            src: 'Images/IMG_6377.jpg',
+            name: 'Indie',
+            title: 'Beautiful Bib',
+            description: 'Take this lovely bad boy for a spin'
+        },
+        {
+            src: 'Images/IMG_6614.jpg',
+            name: 'Indie',
+            title: 'Beautiful Bib',
+            description: 'Take this lovely bad boy for a spin'
+        },
+        {
+            src: 'Images/IMG_6403.jpg',
+            name: 'Indie',
+            title: 'Beautiful Bib',
+            description: 'Take this lovely bad boy for a spin'
+        }]
 });
